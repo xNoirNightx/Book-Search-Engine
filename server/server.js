d2607e08e7034473c96a737312cbd568c6fd9151
Coral <cnight18@hotmail.com>
2023-10-06 23:06:56 -0300

    working through server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express'); //  ApolloServer version 2 heere
+const { authMiddleware } = require('./utils/auth'); 
+const { typeDefs, resolvers } = require('./schemas'); 
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -9,13 +13,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.use(routes);
+//  MongoDB database
+//  authMiddleware to request
+// Apollo Server
+//  Server middleware to Express
+// Serve static assets in production
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`GraphQL Playground available at http://localhost:${PORT}${server.graphqlPath}`);
 });

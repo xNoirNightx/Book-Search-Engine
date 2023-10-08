ed89ce88da936fa4808ffdfd0f11ce70d1506f8f
Coral <cnight18@hotmail.com>
2023-10-08 12:43:12 -0300

    app errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,57 @@
-// import React from 'react';
-// import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-// import { setContext } from "@apollo/client/link/context";
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// import App from './App';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm'; 
+import SignupForm from './components/SignupForm'; 
 
-// // Apollo Client
-// const client = new ApolloClient({
-//   uri: 'http://localhost:3001/graphql', // does link change once added to heroku ? 
-//   cache: new InMemoryCache(),
-// });
+// Apollo Client 
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
 
-// // polloProvider
-// function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//      <SearchBooks>
-//      <SavedBooks>
-//     <ApolloProvider>
-//   );
-// }
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
 
-// export default App;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            <Route exact path='/login' component={LoginForm} /> 
+            <Route exact path='/signup' component={SignupForm} /> 
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;

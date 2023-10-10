46a0441a27dd353b1b7a53ae85a306715bf59681
Coral <cnight18@hotmail.com>
2023-10-10 08:32:41 -0300

    trying to add JAWSDB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -8,16 +8,13 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import SignupForm from './components/SignupForm';
+import process from "process";
 
-// Import process from 'process'
-import process from 'process';
 
-// Apollo Client setup
 const httpLink = createHttpLink({
-  // Conditional URI based on environment
   uri: process.env.NODE_ENV === 'production'
     ? 'https://lil-book-engine-0ed9e9ff8cb3.herokuapp.com/graphql'
-    : 'http://localhost:3000/graphql',
+    : 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
